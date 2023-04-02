b8a35c24f61c77f15091bf6a0ef939fc490d9d3f
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2023-04-03 01:50:50 +0200

    ✨ Add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+/**
+ * @file Storybook configuration.
+ * @author DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
+ */
+import type { StorybookConfig } from '@storybook/vue3-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,19 @@
+/**
+ * @file Storybook preview configuration.
+ * @author DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
+ */
+import type { Preview } from '@storybook/vue3';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
